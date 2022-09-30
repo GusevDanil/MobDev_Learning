44bc2e46a68430ad03d9082b1b38f5f492404a31
v!p3r <gusev_danil_2001@mail.ru>
2022-09-30 16:03:58 +0700

    lab4

--- Lab4/InternetTest/app/src/main/java/com/example/internettest/MainActivity.kt
+++ Lab4/InternetTest/app/src/main/java/com/example/internettest/MainActivity.kt
@@ -4,11 +4,16 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import android.widget.Button
+import okhttp3.OkHttpClient
+import okhttp3.Request
+import okhttp3.Callback
+import okhttp3.Call
+import okhttp3.Response
+import java.io.IOException
 import java.lang.Exception
 import java.net.HttpURLConnection
 import java.lang.Thread
 import java.net.URL
-
 
 class MainActivity : AppCompatActivity() {
 
@@ -28,8 +33,23 @@ class MainActivity : AppCompatActivity() {
                 }
                 catch (e: Exception) { e.printStackTrace() }
                 finally { con.disconnect() }
-            }
+            }.start()
         }
+
+        val reqOkBut = findViewById<Button>(R.id.btnOkHTTP)
+        reqOkBut.setOnClickListener { asyncRequest() }
+    }
+
+    private fun asyncRequest()  {
+        val okHttpClient = OkHttpClient()
+        val request: Request = Request.Builder().url(url).build()
+
+        okHttpClient.newCall(request).enqueue(object : Callback {
+            override fun onFailure(call: Call, e: IOException) {}
+            override fun onResponse(call: Call, response: Response) {
+                val data = response.body?.string().toString()
+                Log.i("Flickr OkCats", data)
+            }
+        })
     }
 }
-
